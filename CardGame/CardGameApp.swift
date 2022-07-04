9bb7f3856263f08eca1e1265a9d4f7b90b791a43
Maksymilian <ts3joker@gmail.com>
2022-07-04 14:25:14 +0200

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Maksymilian Patoń on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
